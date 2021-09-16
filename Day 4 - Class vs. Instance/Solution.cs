8a943b4b263f68e0c6bcfd89c647237c18b565c2
herokudev <ao43085@gmail.com>
2021-09-16 17:34:15 -0600

    Code for challenge#5
    Code for challenge#5

---
+++ Day 4 - Class vs. Instance/Solution.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Person
+{
+    public int age;
+    public Person(int initialAge)
+    {
+        // Add some more code to run some checks on initialAge
+        if (initialAge < 0)
+        {
+            age = 0;
+            Console.WriteLine("Age is not valid, setting age to 0.");
+        }
+        else
+        {
+            age = initialAge;
+        }
+    }
+    public void amIOld()
+    {
+        // Do some computations in here and print out the correct statement to the console 
+        if (age < 13) Console.WriteLine("You are young.");
+        else
+        {
+            if (age < 18) Console.WriteLine("You are a teenager.");
+            else Console.WriteLine("You are old.");
+        }
+    }
+
+    public void yearPasses()
+    {
+        // Increment the age of the person in here
+        age++;
+    }
+
+    static void Main(String[] args)
+    {
+        int T = int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < T; i++)
+        {
+            int age = int.Parse(Console.In.ReadLine());
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++)
+            {
+                p.yearPasses();
+            }
+            p.amIOld();
+            Console.WriteLine();
+        }
+    }
+}
