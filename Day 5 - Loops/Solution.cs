426b8246cbad7db90b941169f9ce038df78fa48c
herokudev <ao43085@gmail.com>
2021-09-24 11:58:44 -0600

    Code for challenge#6
    Code for challenge#6

---
+++ Day 5 - Loops/Solution.cs
@@ -0,0 +1,30 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+            for (int i = 1; i < 11; i++)
+            {
+
+                Console.WriteLine(n + " x " + i + " = " + (n*i));
+
+            }
+    }
+}
