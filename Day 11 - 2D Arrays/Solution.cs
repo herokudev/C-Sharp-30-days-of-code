35abe979c6b88401249d0f6fbd5ff81881e81aee
herokudev <ao43085@gmail.com>
2021-10-07 17:00:56 -0600

    Code for challenge#12
    Code for challenge#12

---
+++ Day 11 - 2D Arrays/Solution.cs
@@ -0,0 +1,44 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[][] arr = new int[6][];
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+        int[] sum = new int[16];
+        int h = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                sum[h] = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1]
+                        + arr[i + 2][j + 2];
+                h++;
+            }
+        }
+
+        Array.Sort(sum);
+        Console.WriteLine(sum[15]);
+    }
+}
