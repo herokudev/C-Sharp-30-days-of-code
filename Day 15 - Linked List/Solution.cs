555a859f5a90a51b180b8d0c1996725dc2f33c35
herokudev <ao43085@gmail.com>
2021-10-21 11:47:14 -0600

    Code for challenge#16
    Code for challenge#16

---
+++ Day 15 - Linked List/Solution.cs
@@ -0,0 +1,58 @@
+using System;
+class Node
+{
+    public int data;
+    public Node next;
+    public Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+
+}
+class Solution
+{
+
+    public static Node insert(Node head, int data)
+    {
+        Node tails = new Node(data);
+        if (head == null)
+        {
+            head = tails;
+        }
+        else
+        {
+            Node start = head;
+            Node parentNode = null;
+            while (start != null)
+            {
+                parentNode = start;
+                start = start.next;
+            }
+            parentNode.next = tails;
+        }
+        return head;
+    }
+
+    public static void display(Node head)
+    {
+        Node start = head;
+        while (start != null)
+        {
+            Console.Write(start.data + " ");
+            start = start.next;
+        }
+    }
+    static void Main(String[] args)
+    {
+
+        Node head = null;
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            head = insert(head, data);
+        }
+        display(head);
+    }
+}
