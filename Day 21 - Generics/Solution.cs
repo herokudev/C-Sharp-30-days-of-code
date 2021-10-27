0082b9c022dfa32dbeccfd6308ef743f9896078e
herokudev <ao43085@gmail.com>
2021-10-26 22:12:19 -0600

    Code for challenge#22
    Code for challenge#22

---
+++ Day 21 - Generics/Solution.cs
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Printer<T>
+{
+
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+
+    // Write your code here
+    public < E > void printArray(E[] inputArray)
+    {
+        for (E element : inputArray)
+        {
+            System.out.printf("%s\n", element);
+        }
+    }
+
+
+}
+
+public class Generics
+{
+
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++)
+        {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++)
+        {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray(intArray);
+        stringPrinter.printArray(stringArray);
+        if (Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+}
+    } 
+}
